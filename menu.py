6cda7b1d9bdb0000444e75e1c2ede266bb04b65c
Hec98 <hector982015@gmail.com>
2021-08-12 23:38:31 -0500

    Menu to return id

--- menu.py
+++ menu.py
@@ -25,3 +25,29 @@ def menu(title, items):
     num = main_menu.show()
     num += 1
     return num
+
+# def dataEnterX():
+#     options = ["entry 1", "entry 2", "entry 3"]
+#     terminal_menu = TerminalMenu(options)
+#     menu_entry_index = terminal_menu.show()
+#     print(f"You have selected {options[menu_entry_index]}!")
+
+def receivedData(title, items):
+    cursor = "❯ "
+    cursor_style = ("fg_cyan", "bold")
+    menu_style = ("fg_gray", "fg_cyan")
+
+    main_menu = TerminalMenu(
+        menu_entries = items,
+        title = f'  {title}',
+        menu_cursor = cursor,
+        menu_cursor_style = cursor_style,
+        menu_highlight_style = menu_style,
+        cycle_cursor = True,
+        clear_screen = True
+    )
+    
+    num = main_menu.show()
+    dat = items[num]
+    return dat
+
